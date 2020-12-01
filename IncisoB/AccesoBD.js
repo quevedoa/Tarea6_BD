91553b5cb8d580efbc496a1f356586496720e6e5
quevedoa <queveduo@gmail.com>
2020-12-01 09:29:29 -0600

    Aqui funciona como el ejemplo en clase

--- IncisoB/AccesoBD.js
+++ IncisoB/AccesoBD.js
@@ -1,10 +1,15 @@
+/*
+Programa ServidorWeb_BD.js
+Muestra el acceso a una base de datos mysql desde el servidor web creado con node.js
+*/
+
 // 1o: Variables requeridas para activar el servidor web local.
 const express = require('express');
 const servidor = express();
 const router = express.Router();    // Se usará para especificar la ruta de la pag. principal
 const path = require('path');   // Para dar dicha ruta
 
- // Conexion a la BD en mysql
+// 2o: Conexión a la BD mysql.
 var mysql = require("mysql");
 var conex = mysql.createConnection({
     host: 'localhost', user: 'usuarioBD', password: 'adminadmin', database: 'PedidosClientes'
@@ -17,6 +22,20 @@ conex.connect(function (err) {
         console.log("Conectado con id: " + conex.threadId);
     }
 });
+
+// 3o: Acceso a la BD: consulta para obtener los datos de los usuarios.
+function getUsuarios() {
+    conex.query('select * from usuarios', [], function (err, result) {
+        if (err) {
+            console.log("Error de conexion: " + err.stack); process.exit(1);
+        } else {
+            result.forEach(function (element) {
+                console.log("RFC: " + element.RFC + "\t Nombre: " + element.Nombre + 
+                    "\t\t Tipo: " + element.Tipo);
+            });
+        }
+    });
+}
 
 // 4o: Servirá para acceder archivos "estaticos" (html, css, etc.)
 servidor.use(express.static(path.join(__dirname,'public')));
@@ -32,26 +51,17 @@ servidor.listen(3000, function() {
     console.log("Terminar con <ctrl><c>");
 });
 
-// “Listar el nombre de los artículos que aparecen en un mínimo de cinco pedidos. 
-// Acompañarlos con la clave y la fecha de los pedidos en los cuales aparecen”.
-var query = "SELECT p.FechaPed, d.FolioP, a.Nombre \
-    from Detalle d, Articulos a, Pedidos p where d.FolioP=p.FolioP and d.IdArt=a.IdArt and d.IdArt in \
-        (select d.IdArt from Detalle d \
-        group by d.IdArt \
-        having count(d.IdArt)>=5) \
-    order by a.Nombre";
+// 6o: Crear la carpeta 'public' en la carpeta raiz del proyecto
+// 
 
-// Alternativa 1
-
-res = [];
-conex.query('select * from usuarios', [], function (err, result) {
-    if (err) {
-        console.log("Error de conexion: " + err.stack); process.exit(1);
-    } else {
-        result.forEach(function (element) {
-            res.push(element);
-        });
-    }
+// 7o: (comentar el llamado a getUsuarios()).
+// Se obtienen los datos de los usuarios; el resultado lo regresa como dato json al servidor de node
+router.get('/incisoBJS', function(req, res) {
+    conex.query('select * from usuarios', [], function (err, result) {
+        if (err) {
+            console.log("Error de conexion: " + err.stack); process.exit(1);
+        } else {    // Env'ia el resultado de la consulta con exito
+            res.status(200).json(result);
+        }
+    });
 });
-
-conex.end;
